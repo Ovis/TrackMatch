3fdee10ef3deebe6fc5bc086fc00bbbed10a1af4
Ovis <3971624+Ovis@users.noreply.github.com>
2026-09-18 01:43:38 +0900

    ファイル操作後Human Verdict方針テストを追加

---
+++ tests/TrackMatch.Core.Tests/HumanVerdictFileOperationPolicyTests.cs
@@ -0,0 +1,15 @@
+﻿using TrackMatch.Core.Candidates;
+using Xunit;
+
+namespace TrackMatch.Core.Tests;
+
+public sealed class HumanVerdictFileOperationPolicyTests
+{
+    [Fact]
+    public void Rename_KeepsVerdict()
+        => Assert.True(HumanVerdictFileOperationPolicy.KeepsVerdictForPathOnlyChange());
+
+    [Fact]
+    public void ContentVerificationFailure_InvalidatesVerdict()
+        => Assert.True(HumanVerdictFileOperationPolicy.InvalidatesVerdictForContentFailure());
+}
